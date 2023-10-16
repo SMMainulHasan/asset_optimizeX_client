67b666d08c1f8182c238763ebc74aec2eb02ce8b
Mainul Hasan <mainulhasan7070@gmail.com>
2023-10-16 22:59:27 +0600

    feat: org button added

--- src/components/container/OrganizationBtn.jsx
+++ src/components/container/OrganizationBtn.jsx
@@ -11,7 +11,7 @@ import { Link, NavLink } from 'react-router-dom'
 import LibraryBtn from './LibraryBtn'
 
 const OrganizationBtn = ({org}) => {
-    const [libraries, setLibraries] = useState([])
+    const [libraries, setLibraries] = useState([]);
     useEffect(()=>{
     axios.get(`/api/library/list/${org.id}/`)
     .then((res) => {
@@ -28,16 +28,22 @@ const OrganizationBtn = ({org}) => {
                     {org.organization_name}
                 </div>
                 <span className="badge border border-blue-600">{!org.role ? "Owner" : org.role }</span>
-                <div className="dropdown dropdown-end dropdown-hover h-12 pt-0">
-                    <label tabIndex={0} className="btn btn-ghost">
-                        <Link to={`/app/org/${org.id}`}><IoMdOptions/></Link>
-                    </label>
-                    <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-md w-52 border border-gray-300">
-                        <li><Link to={""}><CgRename/>Rename</Link></li>
-                        <li><Link to={`/app/org/${org.id}/${org.organization_name}/add-member`}><BsPersonFillAdd/>Add Member</Link></li>
-                        <li><Link to="" className='text-red-500'><MdDelete/>Delete</Link></li>
-                    </ul>
-                </div>
+               
+                    <div className="dropdown dropdown-end dropdown-hover h-12 pt-0">
+                        <label tabIndex={0} className="btn btn-ghost">
+                            <Link to={`/app/org/${org.id}`}><IoMdOptions/></Link>
+                        </label>
+                        {
+                    (!org.role || org.role=="Admin") ? 
+                        <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-md w-52 border border-gray-300">
+                            <li><Link to={""}><CgRename/>Rename</Link></li>
+                            <li><Link to={`/app/org/${org.id}/${org.organization_name}/add-member`}><BsPersonFillAdd/>Add Member</Link></li>
+                            <li><Link to="" className='text-red-500'><MdDelete/>Delete</Link></li>
+                        </ul>
+                            : "" 
+                        }
+                    </div>
+                
             </NavLink>
         </li>
         <li className='ml-4'>
